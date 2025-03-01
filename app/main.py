f0c1f3148ab6fd02d7ce2bccff6a1497c4723bb8
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2025-03-01 11:24:37 -0300

    fix: applying changed from config.yaml and logger chore: fix error on main.py, state doesnt have a retriever. chore: fix async startup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.api.routes.query import router
 from app.services.indexer import index_documents
-from app.config import BOOK_PATH
+from app.services.retriever import DocumentRetriever
+from app.utils.logger import logger
+import asyncio
 import os
-import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-app = FastAPI()
+app = FastAPI(title="Origin of Species RAG")
+
+@app.on_event("startup")
+async def startup():
+    logger.info("Ligando a API RAG")
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, lambda: index_documents("data/origin_of_species.md") if os.path.exists("data/origin_of_species.md") else logger.error("Arquivo do livro não encontrado."))
+    app.state.retriever = DocumentRetriever()
+
 app.include_router(router)
-
-if __name__ =="__main__":
-    if os.path.exists(BOOK_PATH):
-        index_documents(BOOK_PATH)
-    else:
-        print("Arquivo para RAG não encontado. Verifique a existência do arquivo no caminho especificado.")
--- app/services/retriever.py
+++ app/services/retriever.py
@@ -2,18 +2,18 @@ from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain_community.llms import HuggingFacePipeline
 from transformers import pipeline
-from langchain.embeddings import HuggingFaceEmbeddings
-from app.config import DB_DIR, COLLECTION_NAME, LLM_MODEL
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from app.utils.config import config
 import os 
 class DocumentRetriever:
     def __init__(self):
-        self.embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",api_key=os.getenv("HUGGING_FACE_API_KEY"))
+        self.embeddings = HuggingFaceEmbeddings(model_name=config.get_value("models.embedding"))
         self.vector_store = Chroma(
-            persist_directory=DB_DIR,
+            persist_directory=config.get_value("chroma.db_dir"),
             embedding_function=self.embeddings,
-            collection_name=COLLECTION_NAME
+            collection_name=config.get_value("chroma.collection_name")
         )
-        self.llm = HuggingFacePipeline(pipeline=pipeline("text-generation", model=LLM_MODEL, api_key=os.getenv("HUGGING_FACE_API_KEY")))
+        self.llm = HuggingFacePipeline(pipeline=pipeline("text-generation", model=config.get_value("models.llm")))
         self.qa_chain = self._initialize_qa_chain()
 
     def _initialize_qa_chain(self):
@@ -24,21 +24,21 @@ class DocumentRetriever:
             retriever=retriever,
             return_source_documents=True
         )
-    
+
     def query(self, question: str):
         if not question.strip():
             raise ValueError("A pergunta não pode estar vazia.")
-        
+
         result = self.qa_chain({"query": question})
-        
+
         sources = []
         if "source_documents" in result:
             for doc in result["source_documents"]:
-                if hasattr (doc, "metadata") and "source" in doc.metadata:
+                if hasattr(doc, "metadata") and "source" in doc.metadata:
                     source = doc.metadata["source"]
                     if source not in sources:
                         sources.append(source)
-        
+
         return {
             "response": result.get("result", "No answer found."),
             "sources": sources
